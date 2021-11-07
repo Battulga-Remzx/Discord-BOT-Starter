0678bb0957dfdd15fec9a2907993a25b6a69fa63
Glitch (deserted-shimmer-jonquil) <none>
2021-11-07 17:56:04 +0000

    ⏲🎮 Checkpoint ./botConfig.js:36148879/194

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "ODg4MTU5MzM0MDkyMTIwMDg1.YUOo1g.9QnBDqDIIRLgwLZCkra-8l7a7NQ",
+    prefix: "-",
     admins: [
         "PEOPLE WHO CAN USE ADD MONEY (IDS)"
 ],
