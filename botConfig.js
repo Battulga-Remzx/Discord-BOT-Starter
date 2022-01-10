f195955d584358690ab72d122f255a387e402057
Glitch (deserted-shimmer-jonquil) <none>
2022-01-10 11:37:57 +0000

    🚠🛋 Checkpoint ./index.js:36148879/1396 ./server.js:36148879/185 ./config.js:36148879/67 ./package.json:36148879/6

--- botConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    token: "ODg4MTU5MzM0MDkyMTIwMDg1.YUOo1g.9QnBDqDIIRLgwLZCkra-8l7a7NQ",
-    prefix: "-",
-    admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
-],
-    debug: true,
-    countChannel: "countChannelID"
-};
--- index.js
+++ index.js
@@ -1,45 +1,9 @@
-const Discord = require("discord.js");
- const client = new Discord.Client({ disableMentions: 'everyone' });
-const Eco = require("quick.eco");
-client.eco = new Eco.Manager(); // quick.eco
-client.db = Eco.db; // quick.db
-client.config = require("./botConfig");
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-client.shop = {
-  laptop: {
-    cost: 2000
-  },
-  mobile: {
-    cost: 1000
-  },
-  pc: {
-    cost: 3000
-  }
-};
-const fs = require("fs");
+const Discord = require('discord.js')
 
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(f => {
-        if (!f.endsWith(".js")) return;
-        const event = require(`./events/${f}`);
-        let eventName = f.split(".")[0];
-        client.on(eventName, event.bind(null, client));
-    });
-});
+const client = new Discord.Client({})
 
-fs.readdir("./commands/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(f => {
-        if (!f.endsWith(".js")) return;
-        let command = require(`./commands/${f}`);
-        client.commands.set(command.help.name, command);
-        command.help.aliases.forEach(alias => {
-            client.aliases.set(alias, command.help.name);
-        });
-    });
-});
+client.config = require('./config')
 
+client.login(client.config.token)
 
-client.login(client.config.token);
+require('./server')()
